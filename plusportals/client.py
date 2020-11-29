e4193740a0b8050150db3b142ac9edaf742d7951
Dhruv Bisla <bisladhruv@gmail.com>
2020-11-29 09:55:55 -0800

    Set an absolute path to location of credentials cache json file, update client code accordingly

--- plusportals/client.py
+++ plusportals/client.py
@@ -1,18 +1,19 @@
 import requests
 from lxml import html
+import os
 
 from . import credentials
 from . import info
 
 class Client():
-    hasSetCredentials : bool = False
+    hasSetCredentials : bool = os.path.isfile(os.path.join((os.path.dirname(__file__)), 'credentials.json'))
     SCHOOL_NAME : str = None
     EMAIL : str = None
     ID : int = None
     PASSWORD : str = None
     def __init__(self, *args):
         self.session : requests.Session = Client.createSession()
-        (Client.setCredentials(*args), None)[Client.hasSetCredentials]
+        None if Client.hasSetCredentials else Client.setCredentials(*args)
         Client.SCHOOL_NAME = credentials.getCredential('schoolName')
         Client.EMAIL = credentials.getCredential('email')
         Client.ID = credentials.getCredential('ID')
@@ -33,14 +34,14 @@ class Client():
 
     @staticmethod
     def createSession():
-        session = requests.Session()
-        return session        
+        return requests.Session()
 
     def getLanding(self) -> None:
         response = self.session.get(info.LANDING_LOGIN(Client.SCHOOL_NAME))
         self.hasGetLanding = True
+   
     def login(self) -> None:
-        (self.getLanding(), None)[self.hasGetLanding]
+        None if self.hasGetLanding else self.getLanding()
         Data = [
             ('UserName', Client.EMAIL),
             ('Password', Client.PASSWORD),
@@ -48,21 +49,30 @@ class Client():
             ('btnsumit', 'Sign In'),
         ]
         specHeaders = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'cookie': '__cfduid={}; ppschoollink={}; UGUID={}; __RequestVerificationToken={}; _pps=-480'.format(self.session.cookies.get_dict().get("__cfduid"), Client.SCHOOL_NAME, self.session.cookies.get_dict().get("UGUID"), self.session.cookies.get_dict().get("__RequestVerificationToken"))
         }
         response = self.session.post(info.LANDING_LOGIN(Client.SCHOOL_NAME), headers=dict(info.BASE_HEADERS, **specHeaders))
+        print(response.content)
         self.hasLogin = True
+
+
     def getDetails(self) -> None:
-        (self.login(), None)[self.hasLogin]
+        None if self.hasLogin else self.login()
         specHeaders = {
             'cookie': '__cfduid={}; ppschoollink={}; __RequestVerificationToken={}; _pps=-480; ASP.NET_SessionId={}; emailoption=RecentEmails; UGUID={}; ppusername={}; .ASPXAUTH={}'.format(self.session.cookies.get_dict().get('__cfduid'), Client.SCHOOL_NAME, self.session.cookies.get_dict().get('__RequestVerificationToken'), self.session.cookies.get_dict().get('ASP.NET_SessionId'), self.session.cookies.get_dict().get('UGUID'), self.session.cookies.get_dict().get('ppusername'), self.session.cookies.get_dict().get('.ASPXAUTH'))
         }
         response = self.session.post(info.DETAILS(Client.SCHOOL_NAME), headers=dict(info.BASE_HEADERS, **specHeaders))
         tree = html.fromstring(response.text)
-        self.requestVerificationToken = tree.xpath("/html/body/input/@value")[0]
-        self.hasGetDetails = True
+        try:
+            self.requestVerificationToken = tree.xpath("/html/body/input/@value")[0]
+        except Exception as error:
+            raise Exception("Login was not successful")
+        finally:
+            self.hasGetDetails = True
+    
     def getMarkingPeriod(self) -> None:
-        (self.getDetails(), None)[self.hasGetMarkingPeriod]
+        None if self.hasGetMarkingPeriod else self.getDetails()
         specHeaders = {
             'cookie': '__cfduid={}; ppschoollink={}; __RequestVerificationToken={}; _pps=-480; ASP.NET_SessionId={}; emailoption={}; UGUID={}; ppusername={}; .ASPXAUTH={}'.format(self.session.cookies.get_dict().get('__cfduid'), Client.SCHOOL_NAME, self.session.cookies.get_dict().get('__RequestVerificationToken'), self.session.cookies.get_dict().get('ASP.NET_SessionId'), self.session.cookies.get_dict().get('emailoption'), self.session.cookies.get_dict().get('UGUID'), self.session.cookies.get_dict().get('ppusername'), self.session.cookies.get_dict().get('.ASPXAUTH'))
         }
@@ -75,5 +85,6 @@ class Client():
         except Exception as error:
             raise Exception("Invalid marking period response returned: {}".format(error))
         self.hasGetMarkingPeriod
+    
     def getGrades(self):
         pass
--- plusportals/credentials.py
+++ plusportals/credentials.py
@@ -1,13 +1,15 @@
+import os
 import json
+
 
 def setCredentials(schoolName: str, email: str, ID: int, password: str) -> None:
     credentials = {"schoolName": schoolName, "email": email, "ID": ID, "password": password}
-    with open("credentials.json", "w") as f:
+    with open(os.path.join(os.path.dirname(__file__), 'credentials.json'), "w") as f:
         json.dump(credentials, f)
 
 def getCredential(key: str):
     with open("credentials.json", "r") as f:
-        credentials = json.load(f)
+        credentials : dict = json.load(f)
     try:
         return credentials[key]
     except KeyError:
