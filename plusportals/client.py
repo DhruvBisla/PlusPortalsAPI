4b711f8bc631585849ef6f9d6c9c322e619f1b12
Dhruv Bisla <bisladhruv@gmail.com>
2020-12-19 13:51:14 -0800

    Add a pretty print function to print the grades and fix caching of credentials

--- plusportals/client.py
+++ plusportals/client.py
@@ -16,7 +16,7 @@ class Client(session.Session):
     hasCachedCredentials : bool = (os.path.isfile(os.path.join((os.path.dirname(__file__)), 'credentials.json')))
 
     def __init__(self, cacheCredentials : Optional[bool] = False, schoolName : Optional[str] = None, email : Optional[str] = None, ID : Optional[int] = None, password : Optional[str] = None):
-        if cacheCredentials: None if Client.hasCachedCredentials else Client.setCredentials(schoolName, email, ID, password)
+        if cacheCredentials: Client.setCredentials(schoolName, email, ID, password)
         else:
             Client._SCHOOL_NAME = schoolName
             Client._EMAIL = email
@@ -29,6 +29,7 @@ class Client(session.Session):
             Client._PASSWORD = credentials.getCredential('password')
         super().__init__(Client._SCHOOL_NAME, Client._EMAIL, Client._PASSWORD)
         Client.markingPeriods = self.getMarkingPeriods()
+        self.hasGetGrades : bool = False
         self.grades : dict = {}
     
     def reset(self) -> None:
@@ -46,6 +47,15 @@ class Client(session.Session):
             '__requestverificationtoken': '{}'.format(self.requestVerificationToken),
             'cookie': '__cfduid={}; ppschoollink={}; __RequestVerificationToken={}; _pps=-480; ASP.NET_SessionId={}; emailoption={}; UGUID={}; ppusername={}; .ASPXAUTH={}'.format(self.session.cookies.get_dict().get('__cfduid'), Client._SCHOOL_NAME, self.session.cookies.get_dict().get('__RequestVerificationToken'), self.session.cookies.get_dict().get('ASP.NET_SessionId'), self.session.cookies.get_dict().get('emailoption'), self.session.cookies.get_dict().get('UGUID'), self.session.cookies.get_dict().get('ppusername'), self.session.cookies.get_dict().get('.ASPXAUTH'))
         }
-        response = self.session.post(info.GRADES(self.markingPeriods[markingPeriod-1]), headers=dict(info.BASE_HEADERS, **specHeaders))
+        try:
+            response = self.session.post(info.GRADES(self.markingPeriods[markingPeriod-1]), headers=dict(info.BASE_HEADERS, **specHeaders))
+        except:
+            print("Information provided was incorrect; Login was not successful.")
         self.grades = json.loads(response.content.decode('utf-8'))
+        self.hasGetGrades = True
         return response
+        
+    def prettyPrintGrades(self):
+        None if (self.hasGetGrades) else self.getMarkingPeriods()
+        for i in self.grades["Data"]:
+            print("{}'s grade is {}".format(i.get("CourseName")[:(len(i.get("CourseName")))-12],i.get("Average")))
